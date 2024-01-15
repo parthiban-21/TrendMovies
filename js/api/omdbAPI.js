d3415315947117da91574d9b812c878aac3154f1
parthi <parthiban51199@gmail.com>
2024-01-15 11:48:38 +0530

    API

--- js/api/omdbAPI.js
+++ js/api/omdbAPI.js
@@ -1,7 +1,7 @@
 class omdb {
     constructor() {
         this.API_KEY = "7a52001a";
-        this.API_URL = `http://www.omdbapi.com/?apikey=${this.API_KEY}`;
+        this.API_URL = `https://www.omdbapi.com/?apikey=${this.API_KEY}`;
     }
 
     getContentByID(id){
